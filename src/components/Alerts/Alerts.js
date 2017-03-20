469fcb89b2049967787512e2d53e06a54bec639e
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-20 12:18:39 -0700

    Setup component files

---
+++ src/components/Alerts/Alerts.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import './alerts.css';
+
+class Alerts extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+const mapStateToProps = function(store) {
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps)(Alerts);

57d683eb1f7aa0f280c5b4af0ac6764d4a8d7cce
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-24 12:27:24 -0700

    Cleanup resetReports action

--- src/actions/resetReportsAction.js
+++ src/actions/resetReportsAction.js
@@ -4,13 +4,10 @@ export function resetMarkers() {
   return function(dispatch, getState) {
     const state = getState();
     const reports = state.policeReports.reports;
-    console.log(reports);
     const combinedLocations = state.locations.combinedLocations;
     const nextReports = {};
 
     for (const location of combinedLocations) {
-      console.log(location);
-      console.log(reports[`'${location.identity}'`]);
       if (Object.keys(reports[`'${location.identity}'`]).length) {
         nextReports[`'${location.identity}'`] = reports[`'${location.identity}'`];
       }

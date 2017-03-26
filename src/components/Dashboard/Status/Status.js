2d09b660af5b70106c61614c9de01964bf9d408c
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-26 11:59:55 -0700

    Improve status component

--- src/components/Dashboard/Status/Status.js
+++ src/components/Dashboard/Status/Status.js
@@ -7,36 +7,17 @@ import './status.css';
 
 class Status extends Component {
   render() {
-    const name = this.props.selectedLocation.name;
-    const lat = this.props.selectedLocation.lat;
-    const lng = this.props.selectedLocation.lng;
-    let locationCount = this.props.combinedLocations.length;
-    const start = Moment(this.props.timeFilter.startingMilliseconds);
-    const end = Moment(this.props.timeFilter.endingMilliseconds);
-    let diff = Math.round(end.diff(start, 'months', true));
-
-    if (this.props.locationIsSelected) {
-      locationCount = 1;
-    }
-
-    if (diff > 1) {
-      diff += ' months';
-    }
-    else {
-      diff += ' month';
-    }
-
     return (
       <div className="Status-container">
         <h3>What's in this map?</h3>
-        <p>Police incident reports from the last <span className="Status-diff">{diff}</span> within 500 meters of <span className="Status-location">{locationCount} locations</span> in Seattle, WA.
+        <p>Police incident reports from the last <span className="Status-diff">{this.state.diff}</span> within 500 meters of <span className="Status-location">{this.props.combinedLocations.length} locations</span> in Seattle, WA.
         </p>
         {
           this.props.locationIsSelected ?
             <Link
-              to={`/reports/${lat}/${lng}/500/false`}
+              to={`/reports/${this.props.status.state.lat}/${this.props.status.state.lng}/500/false`}
               className="Status-btn">
-              See Incidents Near {name}
+              See Incidents Near {this.props.status.name}
            </Link>
           :
           <div></div>
@@ -51,7 +32,9 @@ const mapStateToProps = function(store) {
     selectedLocation: store.locations.selectedLocation,
     combinedLocations: store.locations.combinedLocations,
     locationIsSelected: store.locations.locationIsSelected,
-    timeFilter:   store.timeFilter
+    timeFilter: store.timeFilter,
+    offenses: store.offenseFilter.fitlerByOffenses,
+    status: store.status
   }
 }
 

dc6cc466e7e323e4628f08f02ed30839afdc2c08
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-27 07:55:04 -0700

    Prepare the Heroku

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
   console.log(path.resolve(__dirname, '..', 'build', 'index.html'));
-  res.sendFile(path.resolve('..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 module.exports = app;

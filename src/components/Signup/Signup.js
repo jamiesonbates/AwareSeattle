225d2f97e10fb7484ffc58b162ae5789768b80f0
Jamieson Bates <jamiesonbates@gmail.com>
2017-03-26 17:39:06 -0700

    Add simple validations to signup form

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -6,12 +6,44 @@ import { signupNewUser } from '../../actions/signupAction';
 import './signup.css';
 
 class Signup extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      error: null
+    }
+  }
   handleSubmit(e) {
     e.preventDefault();
 
     const username = this.refs.username.value;
     const email = this.refs.email.value;
     const password = this.refs.password.value;
+    console.log(password.length);
+
+    if (username.length > 15) {
+      this.setState({
+        error: 'Username must be less than 15 characters.'
+      });
+
+      return;
+    }
+
+    if (password.length < 8) {
+      this.setState({
+        error: 'Password must be longer than 8 characters.'
+      });
+
+      return;
+    }
+
+    if (!email.includes('@')) {
+      this.setState({
+        error: 'Must use valid email.'
+      })
+
+      return;
+    }
 
     this.props.dispatch(signupNewUser(username, email, password));
     this.refs.signupForm.reset();
@@ -36,6 +68,15 @@ class Signup extends Component {
             <label>Password</label>
             <input type="text" ref="password" />
           </div>
+
+          {
+            this.state.error ?
+              <div className="Signup-error">
+                <p>{this.state.error}</p>
+              </div>
+            :
+              null
+          }
 
           <button type="submit">Signup</button>
         </form>

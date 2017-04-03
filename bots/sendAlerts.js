e851c34368bec4b17e00c6b77309902781bb6f67
Jamieson Bates <jamiesonbates@gmail.com>
2017-04-02 17:21:46 -0700

    Adjust alerts api call to return specific columns across 4 tables

--- bots/sendAlerts.js
+++ bots/sendAlerts.js
@@ -11,12 +11,15 @@ const domain = 'sandbox2a79e8a13e9b429486e37c1dc03de4d8.mailgun.org';
 // Get all alerts + matching user from DB
 const getAllAlerts = function() {
   return knex('alerts')
+    .select('users.id as userId', 'alerts.id as alertId', 'user_alert_locations.id as locationId', 'offense_types.id as offenseTypeId', 'offense_types.offense_name as offenseName', 'alerts.range as range', 'users.email as email', 'users.username as username', 'user_alert_locations.location_title as location', 'user_alert_locations.location as address', 'user_alert_locations.lat as lat', 'user_alert_locations.lng as lng')
     .innerJoin('users', 'alerts.user_id', 'users.id')
-    .innerJoin('user_alert_locations', 'alerts.user_alert_location_id', 'user_alert_locations.id');
+    .innerJoin('user_alert_locations', 'alerts.user_alert_location_id', 'user_alert_locations.id')
+    .innerJoin('offense_types', 'offense_types.id', 'alerts.offense_type_id')
 }
 
 // For each alert, check whether new data is within the alert's given radius
 const checkForMatches = function(alert) {
+  console.log('matches api call', alert);
   const promise = new Promise((resolve, reject) => {
     return knex.raw(`
       SELECT *
@@ -24,6 +27,7 @@ const checkForMatches = function(alert) {
       WHERE ST_DWithin(police_reports.location, ST_POINT(${parseFloat(alert.lng)}, ${parseFloat(alert.lat)}), ${alert.range}) AND police_reports.new = true AND police_reports.offense_type_id = ${alert.offense_type_id};
     `)
     .then((data) => {
+      console.log('api call', data.rows);
       if (data.rows.length) {
         const mergedData = alert;
         mergedData.reports = data.rows;
@@ -69,6 +73,7 @@ const sendAlertsFromMatches = function(matchesByUser) {
 const sendAlertsJob = function() {
   getAllAlerts()
     .then((alerts) => {
+      console.log(alerts);
       const res = [];
 
       for (const alert of alerts) {
@@ -78,6 +83,7 @@ const sendAlertsJob = function() {
       return Promise.all(res);
     })
     .then((data) => {
+      console.log('data', data);
       const matches = [];
       for (const result of data) {
         if (result) {
@@ -91,10 +97,9 @@ const sendAlertsJob = function() {
       return mapMatchesToUsers(matches);
     })
     .then((matchesByUser) => {
-      for (const report in matchesByUser.reports) {
-        // console.log(matchesByUser.reports[report]);
-      }
+      console.log(matchesByUser);
       for (const user in matchesByUser) {
+        console.log(user.reports);
         const mailgun = new Mailgun({ apiKey, domain });
 
         const data = {
